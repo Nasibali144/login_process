20b5b5460e0906c62126a323ed500929cd257e05
Nasibali <abdiyevnasibali@gmail.com>
2022-06-03 16:39:36 +0500

    commit 3: UI Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -107,7 +107,11 @@ class _LoginPageState extends State<LoginPage> {
                   height: 50,
                   width: double.infinity,
                   child: TextFormField(
+                    textAlignVertical: TextAlignVertical.center,
                     decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+                      alignLabelWithHint: true,
+                      isCollapsed: true,
                       filled: true,
                       fillColor: AppConstants.colorField,
                       border: OutlineInputBorder(
@@ -126,7 +130,7 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       errorStyle: const TextStyle(color: AppConstants.colorError,),
                       // errorText: "Invalid email address",
-                      suffixIcon: const Icon(Icons.close, color: AppConstants.colorTypingText, size: 16,)
+                      suffixIcon: const Icon(Icons.close, color: AppConstants.colorTypingText, size: 16,),
                     ),
                     style: const TextStyle(
                       color: AppConstants.colorTypingText,
@@ -134,13 +138,177 @@ class _LoginPageState extends State<LoginPage> {
                       fontFamily: "Inter",
                       fontWeight: FontWeight.w500,
                     ),
+                    cursorColor: AppConstants.colorTextButton,
+                    cursorWidth: 2,
+                    cursorHeight: 18,
                   ),
                 ),
+                const SizedBox(height: 20,),
+
+                // #password
+                const Text(
+                  "Password",
+                  style:  TextStyle(
+                    fontSize: 13,
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w500,
+                    color: AppConstants.colorText,
+                  ),
+                ),
+                const SizedBox(height: 5,),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: TextFormField(
+                    textAlignVertical: TextAlignVertical.center,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+                      alignLabelWithHint: true,
+                      isCollapsed: true,
+                      filled: true,
+                      fillColor: AppConstants.colorField,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: AppConstants.colorTextFieldBorder, width: 1),
+                      ),
+                      hintText: "Your password",
+                      hintStyle: const TextStyle(color: AppConstants.colorText,),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: AppConstants.colorTextFieldBorder, width: 1),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: AppConstants.colorError, width: 1),
+                      ),
+                      errorStyle: const TextStyle(color: AppConstants.colorError,),
+                      // errorText: "Invalid email address",
+                      suffixIcon: const Icon(Icons.close, color: AppConstants.colorTypingText, size: 16,),
+                    ),
+                    style: const TextStyle(
+                      color: AppConstants.colorTypingText,
+                      fontSize: 13,
+                      fontFamily: "Inter",
+                      fontWeight: FontWeight.w500,
+                    ),
+                    cursorColor: AppConstants.colorTextButton,
+                    cursorWidth: 2,
+                    cursorHeight: 18,
+                    obscureText: true,
+                    obscuringCharacter: "*",
+                  ),
+                ),
+                const SizedBox(height: 40,),
+
+                // #sign_in
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: AppConstants.colorMainButton,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: "Inter",
+                    ),
+                    fixedSize: Size(MediaQuery.of(context).size.width, 60),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign In",
+                  ),
+                ),
+                const SizedBox(height: 10,),
+
+                // #forgot_password
+                SizedBox(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Forgot password?", style: TextStyle(
+                        color: AppConstants.colorTextButton,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: "Inter",
+                      ),
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
 
             // #footer
-            Column(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children:  [
+                // #sign_in_with
+                Text(
+                  "Sign in with",
+                  style: TextStyle(
+                  color: AppConstants.colorText,
+                  fontFamily: "Inter",
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500
+                ),
+                ),
+                SizedBox(height: 15,),
+
+                // #icons
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        alignment: Alignment.center,
+                        clipBehavior: Clip.antiAlias,
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppConstants.colorIconButton,
+                        ),
+                        child: SvgPicture.asset("assets/icons/ic_apple.svg", fit: BoxFit.cover,),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        alignment: Alignment.center,
+                        clipBehavior: Clip.antiAlias,
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppConstants.colorIconButton,
+                        ),
+                        child: SvgPicture.asset("assets/icons/ic_meta.svg", fit: BoxFit.cover,),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        alignment: Alignment.center,
+                        clipBehavior: Clip.antiAlias,
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppConstants.colorIconButton,
+                        ),
+                        child: SvgPicture.asset("assets/icons/ic_twitter.svg", fit: BoxFit.cover,),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ],
         ),
       ),
